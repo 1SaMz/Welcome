3df24ebfefcf61d481e4ece1dada2a704b7e4158
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-07 23:51:08 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("509745217285652513");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`/me ** , # Welcome To __Magic__ Server , :mg: :leaves: **
+`), 4000)        
+}
+});
+
+
+client.login(process.env.BOT_TOKEN);//
+
+
+
