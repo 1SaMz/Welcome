5801fae8bdb9629a15d0ec30e0a5e38877124b91
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-30 21:47:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("509745217285652513");
+const mohamed= member.guild.channels.get("516661672040202241");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**, # Welcome To __Magic__ Server** ,:leaves:`), 4000)        
